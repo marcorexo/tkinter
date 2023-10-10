0f88af5df88cdd61caebb5f3a383bf2a8d021adc
Marc Edwards <dyfroedd@gmail.com>
2023-10-10 17:07:10 +0100

    first commit

---
+++ miles_to_kilometers/main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+window  = tk.Tk()
+window.title('Demo')
+window.geometry('300x150')
+
+#title
+title_label = ttk.Label(window, text='Miles to Kilomters', font='Calibri 24 bold')
+title_label.pack()
+
+#input field
+input_frame = ttk.Frame(master=window)
+entry = ttk.Entry(master=input_frame)
+button = ttk.Button(master=input_frame, text='Convert')
+
+entry.pack(side='left', padx=10)
+button.pack(side='left')
+input_frame.pack(pady=10)
+
+output_label = ttk.Label(master=window, text='output...', font='Calibri 24 bold')
+output_label.pack()
+
+window.mainloop()
