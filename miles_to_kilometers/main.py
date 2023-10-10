f800eb6bfd6bde4d1db522d87f92390b1808d944
Marc Edwards <dyfroedd@gmail.com>
2023-10-10 19:59:28 +0100

    completed

--- miles_to_kilometers/main.py
+++ miles_to_kilometers/main.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk
 
+def convert():
+    km = entry_val.get() * 1.60934
+    output_string.set(km)
+    #entryVal = km
+    
+
 window  = tk.Tk()
 window.title('Demo')
 window.geometry('300x150')
@@ -11,14 +17,19 @@ title_label.pack()
 
 #input field
 input_frame = ttk.Frame(master=window)
-entry = ttk.Entry(master=input_frame)
-button = ttk.Button(master=input_frame, text='Convert')
+entry_val = tk.DoubleVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_val)
+button = ttk.Button(master=input_frame, text='Convert', command=convert)
 
 entry.pack(side='left', padx=10)
 button.pack(side='left')
 input_frame.pack(pady=10)
 
-output_label = ttk.Label(master=window, text='output...', font='Calibri 24')
+output_string = tk.StringVar()
+output_label = ttk.Label(master=window, 
+                         text='output...', 
+                         font='Calibri 24', 
+                         textvariable=output_string)
 output_label.pack(pady=5)
 
 window.mainloop()
